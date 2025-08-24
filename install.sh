8332a6603b1c7cb788cc6be4192cda4b411dcec2
Skaza2 <skazajiri@gmail.com>
2025-08-24 13:58:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,11 +19,11 @@ mkdir -p "$datova_slozka"
 # 2. Vytvorenie prveho testovacieho suboru
 touch "$datova_slozka/1.txt"
 
-echo "name="Projekt"" >> $datova_slozka/1.txt
+echo 'name="Projekt"' >> $datova_slozka/1.txt
 echo "priority=100" >> $datova_slozka/1.txt
-echo "due_date="10.08.2025"" >> $datova_slozka/1.txt
+echo 'due_date="10.08.2025"' >> $datova_slozka/1.txt
 echo "status=Rozpracovany" >> $datova_slozka/1.txt
-echo "description="Je to dokonale"" >> $datova_slozka/1.txt
+echo 'description="Je to dokonale"' >> $datova_slozka/1.txt
 
 # 3. Vytvorenie priečinka pre aplikáciu a pre ukladanie dát v nej
 cp -r src/* "$slozka"
